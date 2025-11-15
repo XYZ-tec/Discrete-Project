95b256ef80b715c8a8b25af6860c1d328b122e04
Musferah <musferahnasar386@gmail.com>
2025-11-15 16:44:36 +0500

    For Managing The Semester System

---
+++ Discrete End Sem/Semester.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef SEMESTER_H
+#define SEMESTER_H
+#include <iostream>
+#include <string>
+using namespace std;
+class Semester 
+{
+private:
+    string id;
+    string name;  // e.g Fall 2023
+    string startDate;
+    string endDate;
+public:
+    Semester(string id, string name, string start, string end);
+    string getId() const;
+    string getName() const;
+    string getStartDate() const;
+    string getEndDate() const;
+    void display() const;
+};
+#endif
