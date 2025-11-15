093a177259632e0b252767216715de6cbc243700
Musferah <musferahnasar386@gmail.com>
2025-11-15 16:07:14 +0500

    The Starting Structure for the Faculty Class

---
+++ Discrete End Sem/Faculty.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef FACULTY_H
+#define FACULTY_H
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Faculty
+{
+private:
+    string id;
+    string name;
+    string department;
+    vector<string> assignedCourses;  // Set of assigned course IDs
+
+public:
+    Faculty(string id, string name, string dept);
+    string getId() const;
+    string getName() const;
+    string getDepartment() const;
+    void assignCourse(string courseId);
+    const vector<string>& getAssignedCourses() const;
+    void display() const;
+};
+
+#endif
