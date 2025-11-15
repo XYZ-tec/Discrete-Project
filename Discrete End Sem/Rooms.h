b7dfdf3f070d7aac1d18a229b8217d9f80208a9d
Musferah <musferahnasar386@gmail.com>
2025-11-15 16:15:30 +0500

    Basic Structure for the Allocation and Managing Rooms

---
+++ Discrete End Sem/Rooms.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef ROOMS_H
+#define ROOMS_H
+#include<iostream>
+#include <string>
+using namespace std;
+class Rooms 
+{
+private:
+    string id;
+    string type;  // e.g Lecture or Lab etc
+    int capacity;
+
+public:
+    Rooms(string id, string type, int capacity);
+    string getId() const;
+    string getType() const;
+    int getCapacity() const;
+    void display() const;
+};
+
+#endif
