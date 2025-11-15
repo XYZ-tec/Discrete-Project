6cbaa5d18952da2a17de6fdab3a27b26a4ea3b17
Musferah <musferahnasar386@gmail.com>
2025-11-15 16:01:17 +0500

    Starter Structure for the Course class

---
+++ Discrete End Sem/Courses.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef COURSES_H
+#define COURSES_H
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+class Courses 
+{
+private:
+    string id;
+    string name;
+    int credits;
+    vector<string> prerequisites;  // Set of prerequisite course IDs 
+
+public:
+    Courses(string id, string name, int credits);
+    string getId() const;
+    string getName() const;
+    int getCredits() const;
+    void addPrerequisite(string prereqId);
+    const vector<string>& getPrerequisites() const;
+    void display() const;
+};
+#endif
